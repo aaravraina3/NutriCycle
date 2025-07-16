747ac1a48ee460e9506822185a296affbb452f57
Aarav Raina <aaravraina2024@gmail.com>
2025-07-16 02:29:48 -0400

    Add working Flask backend and HTML frontend

---
+++ backend/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, send_from_directory
+import sqlite3
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='static')
+CORS(app)
+
+@app.route('/')
+def home():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    return jsonify({"message": "Backend is running!"})
+
+@app.route('/api/log', methods=['POST'])
+def log_compost():
+    data = request.get_json()
+    weight = data.get('weight')
+    location = data.get('location')
+
+    conn = sqlite3.connect('compost.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO compost_logs (weight, location)
+        VALUES (?, ?)
+    ''', (weight, location))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"status": "success", "weight": weight, "location": location})
+
+@app.route('/api/logs', methods=['GET'])
+def get_logs():
+    conn = sqlite3.connect('compost.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM compost_logs')
+    logs = cursor.fetchall()
+    conn.close()
+
+    return jsonify([{
+        "id": log[0],
+        "weight": log[1],
+        "location": log[2],
+        "timestamp": log[3]
+    } for log in logs])
+
+if __name__ == '__main__':
+    app.run(debug=True)
